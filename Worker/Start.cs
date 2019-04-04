efb93b9181a4dcb791489c1357022881f5abf771
Andriy <Andriy@DESKTOP-9LBFKE8>
2019-04-04 20:09:07 +0300

    Add project files.

---
+++ Worker/Start.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Worker
+{
+    public partial class Start : Form
+    {
+        private string path, filename;
+        public Start()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    MessageBox.Show("Directory:" + fbd.SelectedPath);
+                    path = fbd.SelectedPath;
+                }
+            }
+            filename = Microsoft.VisualBasic.Interaction.InputBox("Enter name of file (Without .excel) ", "Choosing name", "IAMSAMPLE");
+           
+        }
+    }
+}
