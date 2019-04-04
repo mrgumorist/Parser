f4f2b4971055e3a0d26e44196a733c12b8766315
Andriy <Andriy@DESKTOP-9LBFKE8>
2019-04-04 22:38:26 +0300

    gerger

---
+++ Worker/House.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worker
+{
+    public class House
+    {
+        string Name { get; set; }
+        string Adress { get; set; }
+        string Price { get; set; }
+        string CountOfRooms { get; set; }
+        string Metrazh { get; set; }
+        string Link { get; set; }
+    }
+}
--- Worker/Start.cs
+++ Worker/Start.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,7 +16,8 @@ namespace Worker
 {
     public partial class Start : Form
     {
-        private string path, filename;
+        public static string path, filename, Link;
+       
         public Start()
         {
             InitializeComponent();
@@ -25,7 +29,34 @@ namespace Worker
         }
         public static void InstanceMethod()
         {
+            string htmlCode;
+            double countofpages;
             MessageBox.Show("We are start our parsing");
+            using (WebClient client = new WebClient()) // WebClient class inherits IDisposable
+            {
+                client.Encoding = Encoding.UTF8;
+                 htmlCode = client.DownloadString(Link);
+                File.WriteAllText("first.txt", htmlCode);
+            }
+            //< span class="jss122">867 объявлений</span>
+            string patern = @"jss122"">(.*?)</span>";
+            Match match = Regex.Match(htmlCode, patern);
+            string strcount = match.Value;
+            strcount=strcount.Replace(@"jss122"">", "");
+           // MessageBox.Show(strcount);
+            int value = 0;
+            foreach (char c in strcount)
+            {
+                if ((c >= '0') && (c <= '9'))
+                {
+                    value = value * 10 + (c - '0');
+                }
+            }
+            //MessageBox.Show(value.ToString()) ;
+            countofpages = value / 30;
+            MessageBox.Show(countofpages.ToString()) ;
+            MessageBox.Show("We are end our parsing");
+            
         }
         private void button1_Click(object sender, EventArgs e)
         {
@@ -42,6 +73,7 @@ namespace Worker
             filename = Microsoft.VisualBasic.Interaction.InputBox("Enter name of file (Without .excel) ", "Choosing name", "IAMSAMPLE");
             Thread InstanceCaller = new Thread(
             new ThreadStart(InstanceMethod));
+            Link = textBox1.Text;
 
             // Start the thread.
             InstanceCaller.Start();
